9483615fb15bf271de30395b3f7cac0c44416028
Vadzim <omoegod@icloud.com>
2020-12-18 10:59:39 +0300

    Edit

--- telegabot.py
+++ telegabot.py
@@ -1,9 +1,10 @@
 import telebot
+import config
 
-bot = telebot.OmoegodBot('1409141931:AAEIioEkKLK3pjrjRacBPjS5Z0Z6b04XZK0')
+bot = telebot.TeleBot('1409141931:AAEIioEkKLK3pjrjRacBPjS5Z0Z6b04XZK0')
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
- bot.polling()   
+ bot.polling(none_stop=True)
